39600fdce6d8997529bda2e31d81f5c467de4948
Ruban <rubandavid88@gmail.com>
2020-02-01 12:54:19 +0000

    fghg

--- src/main/java/com/example/petpeers/controllers/LoginController.java
+++ src/main/java/com/example/petpeers/controllers/LoginController.java
@@ -2,20 +2,23 @@ package com.example.petpeers.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.petpeers.service.LoginService;
 
-@Controller
+@RestController
+@CrossOrigin
 public class LoginController {
 
 	@Autowired
 	private LoginService loginService;
 	
-	@PostMapping
+	@GetMapping("/login")
 	public String login(@RequestParam String name , @RequestParam String password) {
 		return loginService.validate(name,password);
 	}
